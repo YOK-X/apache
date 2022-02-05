10d7ce358a447d6839951c6cc2fac1777a00e14f
yok <info.autonika@gmail.com>
2022-02-05 15:53:08 +0200

    1

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -42,5 +42,5 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 2.0.1"
+  version                = ">= 2.0.1"
 }
