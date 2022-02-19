b8f50ca9ed07094f64ce48ce05c1356b3e2e0efa
yok <info.autonika@gmail.com>
2022-02-19 22:19:07 +0200

    1

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    profile = "Administrator"
+    bucket = "my-terraform-states-s3"
+    encrypt = true
+    key    = "AWS/Dev/terraform-states/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
